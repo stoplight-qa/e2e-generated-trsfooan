4e4619a8d14f159a517604fa54f8ae05be679f3c
qa-vrmrk-3-uyid@example.com <qa-vrmrk-3-uyid@example.com>
2023-03-24 19:00:53 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjE1MDAyNA/spectral.js?branch_slug=main&token=cf996dee-17a1-4468-91e8-6776f3ca1b26";
+export default { extends: ruleset };
